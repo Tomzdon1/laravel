8fae46287331b7d482dfa123a8106c6851c5d829
dalek <dalek@5TDVW02.europa-tu.local>
2015-11-30 10:23:51 +0100

    Poprawka związana z importem polis

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,10 +12,12 @@
 */
 
 $app->group([
-    'prefix' => 'travel/v1', 
+    'prefix' => 'cp/travel/v1', 
     'middleware' => 'request_validate:'.env('API_DEFINITION_TRAVEL_V1').','.env('ERROR_MODEL_IMPL_TRAVEL_V1'), 
     'namespace' => 'App\apiModels\travel\v1\Controllers'
     ], function ($app) {
         $app->post('get_quotes','getQuotesCtrl@request');
         $app->post('calculate_policy','calculatePolicyCtrl@request');
+        $app->post('import_policies','importPoliciesCtrl@request');
+
 });
