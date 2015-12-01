f823884ebb6540a0721ab74245f6f12291865b77
roznowski <roznowski@PRROZ3.europa-tu.local>
2015-12-01 14:34:16 +0100

    Dodanie grupy tools w której będzie między innymi funkcja echo

--- app/apiModels/Tools/echoCtrl.php
+++ app/apiModels/Tools/echoCtrl.php
@@ -7,6 +7,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 use App\Http\Controllers\RequestCtrl;
 use Illuminate\Http\Request;
 //use Symfony\Component\HttpFoundation\Response as Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
 
@@ -16,13 +17,14 @@ class echoCtrl extends  BaseController{
 
   public function request(Request $request)    
   {
-      Log::critical('File='.__FILE__);        
+      Log::debug('File='.__FILE__);        
       
       $content_xml = file_get_contents("C:\\Projekty\\PHP\\PrintOut\\Dane\\dane.xml");
       $template_name = "TESTCOM";
       $pdf =  app()->make('PrintOut')->PrintSingleFile( $template_name,$content_xml);
        
       return (new Response($pdf))->header('Content-Type','application/pdf');
+      //return new JsonResponse(["data"=>"data"]);
       //return \DateTime::createFromFormat('U.u', microtime(true))->format("YmdHisu");
     }
 
