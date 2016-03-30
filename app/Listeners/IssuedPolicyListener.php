3f65b64dfd16ac6ffbd979db57cebdccec6da035
Tomasz Duda <tomasz.duda@tueuropa.pl>
2016-03-30 15:51:04 +0200

    Dodanie nazwy i wersji api do koperty

--- app/Listeners/IssuedPolicyListener.php
+++ app/Listeners/IssuedPolicyListener.php
@@ -3,7 +3,7 @@
 namespace App\Listeners;
 
 use App\Events\IssuedPolicyEvent;
-use App\apiModels\internal\v1\ENVELOPE;
+use App\apiModels\internal\v1 as internal;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -30,14 +30,20 @@ class IssuedPolicyListener
 
         $event->policy->product['company'] = 'M';
 
-        $envelope = new ENVELOPE();
-        $envelope->setBody($event->policy);
+        $api = new internal\API();
+        $api->setName(array_keys($event->policy->partnerData['apis'])[0]);
+        $api->setVersion($event->policy->partnerData['apis'][$api->getName()]['version']);
+
+        $envelope = new internal\ENVELOPE();
         $envelope->setType('policy');
+        $envelope->setApi($api);
+        $envelope->setStatus($event->policy->getStatus());
         $envelope->setCompany($event->policy->product['company']);
         $envelope->setSrcId($event->policy->policyId->{'$id'});
-        $envelope->setSendDT();
+        $envelope->setSendDT(new \DateTime());
+        $envelope->setBody($event->policy);
 
-        app('queue')->pushRaw($envelope->encode());
+        app('queue')->pushRaw($envelope);
 
         app('log')->debug('End IssuedPolicyListener');
     }
