a6122d93583c055c2cf672d3fb250470db92cf93
Tomasz Duda <tomasz.duda@tueuropa.pl>
2016-06-03 16:23:36 +0200

    Walidacje biznesowe dla danych osobowych

--- app/apiModels/travel/v1/implementations/PERSONDATA_impl.php
+++ app/apiModels/travel/v1/implementations/PERSONDATA_impl.php
@@ -23,18 +23,12 @@ class PERSONDATA_impl extends PERSONDATA
      * @var array
      */
     public static $validators = [
-        'birth_date' => 'before:today',
+        'birth_date' => 'required_if:type,private|before:today',
         'pesel' => 'pesel',
         'nationality' => 'countryCode',
+        'business_name' => 'required_if:type,sole_trader,company',
+        'nip' => 'required_if:type,sole_trader,company'
     ];
-    public static $swaggerTypes = array(
-        'first_name' => 'string',
-        'last_name' => 'string',
-        'birth_date' => '\DateTime',
-        'pesel' => 'string',
-        'nationality' => 'string',
-        'document_no' => 'string'
-    );
 
     /**
      * Constructor
@@ -47,6 +41,6 @@ class PERSONDATA_impl extends PERSONDATA
 
     public function getBirthDate()
     {
-        return $this->birth_date->format('Y-m-d');
+        return $this->birth_date instanceof DateTime ? $this->birth_date->format('Y-m-d') : $this->birth_date;
     }
 }
