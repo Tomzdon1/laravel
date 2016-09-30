b52ef5ff3e5cefa6fb4918c19c0892751e07f4a6
Tomasz Duda <tomasz.duda@tueuropa.pl>
2016-09-30 16:58:22 +0200

    Pobieranie wiadomości z kolejki i przydzielanie do odpowiedniego zadania

---
+++ app/Jobs/ConsumeCPQueue.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Jobs;
+
+// @ todo poprawic nazwe w zaleznosci od kolekcji
+class ConsumeCPQueue extends Job
+{
+    protected $podcast;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($podcast=null)
+    {
+        $this->podcast = $podcast;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        app('log')->debug('Run job ConsumeCPQueue');
+        
+        app('Amqp')->consume('sub.out.all', function ($message, $resolver) {
+            $envelope = json_decode($message->body);
+           
+            if ($envelope) {
+                switch ($envelope->type) {
+                    case 'sms-status':
+                        $resolver->acknowledge($message);
+                        dispatch(new \App\Jobs\SaveSMSConfirmation($message->body));
+                        break;
+                    case 'policy-status':
+                        $resolver->acknowledge($message);
+                        dispatch(new \App\Jobs\SaveIssuedPolicyConfirmation($message->body));
+                        break;
+                }
+            }
+            
+            // uncomment if want to stop and run again (depends on sheduler) consumer after processed
+            // $resolver->stopWhenProcessed();
+
+        }, [
+            'persistent' => true, // required if you want to listen forever
+        ]);
+    }
+}
