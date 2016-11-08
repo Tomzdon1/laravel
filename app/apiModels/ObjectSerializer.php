dabb21eb9f5e4bade87b372a6c7cf214aa94b933
Tomasz Duda <tomasz.duda@tueuropa.pl>
2016-11-08 09:36:18 +0100

    Fix package name

--- app/apiModels/ObjectSerializer.php
+++ app/apiModels/ObjectSerializer.php
@@ -5,7 +5,7 @@
  * PHP version 5
  *
  * @category Class
- * @package  Swagger\Client
+ * @package  App\apiModels\internal\v2
  * @author   http://github.com/swagger-api/swagger-codegen
  * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
  * @link     https://github.com/swagger-api/swagger-codegen
@@ -37,7 +37,7 @@ namespace App\apiModels;
  * ObjectSerializer Class Doc Comment
  *
  * @category Class
- * @package  Swagger\Client 
+ * @package  App\apiModels\internal\v2 
  * @author   http://github.com/swagger-api/swagger-codegen
  * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache Licene v2
  * @link     https://github.com/swagger-api/swagger-codegen
